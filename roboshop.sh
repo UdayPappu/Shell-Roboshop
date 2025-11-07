e0820ae949b6ae51754e0722a744a2035d1d2276
Uday Pappu <saiudaybhaskarpappu@gmail.com>
2025-11-07 19:08:51 +0530

    update files

--- roboshop.sh
+++ roboshop.sh
@@ -23,3 +23,22 @@ INSTANCE_ID=$(aws ec2 run-instances   --image-id ami-09c813fb71547fc4f   --insta
      echo "$instance IP address is $IP"
 
 done
+
+aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '{
+    "Comment": "Creating or updating a record set for Cognito endpoint",
+    "Changes": [
+      {
+        "Action"            : "CREATE",
+        "ResourceRecordSet" : {
+          "Name"            : "'$recordname'.mydoamin.com.",
+          "Type"            : "CNAME",
+          "TTL"             : 120,
+          "ResourceRecords" : [
+            { "Value": "'$recordvalue'" }
+          ]
+        }
+      }
+    ]
+  }'

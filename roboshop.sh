65b31fe3999a374ece65658a0ba5c052c2075bcb
Uday Pappu <saiudaybhaskarpappu@gmail.com>
2025-11-07 19:25:55 +0530

    update files

--- roboshop.sh
+++ roboshop.sh
@@ -9,7 +9,7 @@ DOMAIN_NAME="udaypappu.fun"
 
 for instance in ${INSTANCES[@]}
 do
-INSTANCE_ID=$(aws ec2 run-instances   --image-id ami-09c813fb71547fc4f   --instance-type t3.micro  --security-group-ids sg-075219685ee98df3d   --tag-specifications "ResourceType=instance,
+INSTANCE_ID=$(aws ec2 run-instances   --image-id ami-09c813fb71547fc4f   --instance-type t2.micro  --security-group-ids sg-075219685ee98df3d   --tag-specifications "ResourceType=instance,
   Tags=[{Key=Name,Value=$instance}]"   --query "Instances[0].InstanceId"   --output text)
   if [ $instance != "frontend" ]
   then
